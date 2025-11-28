e04891440b0e9770701e1ad50d24352a0d21bea4
cancan <nazli1453@gmail.com>
2025-11-28 16:22:18 +0300

    nazli degisiklik yapti

--- lib/feature/notification/notification_screen.dart
+++ lib/feature/notification/notification_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobil_proje/feature/login/login_screen.dart';
 
 class NotificationScreen extends StatelessWidget {
   const NotificationScreen({Key? key}) : super(key: key);
@@ -15,6 +16,14 @@ class NotificationScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text('Notification'),
+          TextButton(onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              var loginScreen = LoginScreen();
+              return loginScreen;
+            }));
+          }, child: Text('Geri Dön')),
+             Text("data")
+          
         ],
       ),
     );
